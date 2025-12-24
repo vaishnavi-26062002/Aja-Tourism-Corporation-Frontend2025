9f3d90e8167bfe8f4a219bc75a9c37ff60fd0894
manishm72 <m.maharana@ajacs.in>
2025-12-24 16:45:12 +0530

    update

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const AdminLogin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
